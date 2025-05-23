17e511d82e3ca3c5e5e1cc1068988f77e9b27610
JIN_DDIT <zanalyst01@gmail.com>
2025-05-23 17:33:59 +0900

    feat: MVVM CRUD complete
    axios

--- daoemp.js
+++ daoemp.js
@@ -23,6 +23,52 @@ class DaoEmp {
     return list;
   }
 
+  selectOne(e_id) {
+    var sql = `
+      select
+        e_id, e_name, gen, addr
+      from
+        emp
+      where
+        e_id = ${e_id}
+    `;
+    const list = this.conn.query(sql);
+
+    return list.length > 0 ? list[0] : null;
+  }
+
+  update(vo) {
+    var sql = `
+      UPDATE EMP 
+      SET 
+        e_name = ${vo.e_name},
+        gen = ${vo.gen},
+        addr = ${vo.addr}
+      WHERE 
+        e_id = ${vo.e_id}
+    `;
+    const result = this.conn.query(sql);
+    return result.affectedRows;
+  }
+
+  delete(e_id) {
+    var sql = `
+      DELETE FROM emp 
+      WHERE e_id = ${e_id}
+    `;
+    const result = this.conn.query(sql);
+    return result.affectedRows;
+  }
+
+  insert(vo) {
+    var sql = `
+      INSERT INTO emp (e_id, e_name, gen, addr) 
+      VALUES ('${vo.e_id}', '${vo.e_name}', '${vo.gen}', '${vo.addr}')
+    `;
+    const result = this.conn.query(sql);
+    return result.affectedRows;
+  }
+
 }
 
 module.exports = DaoEmp;
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs'); //
 app.engine('html', require('ejs').renderFile); //html 렌더 가능
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // 정적 폴더 나누기
@@ -31,6 +31,27 @@ app.get('/index.do', (req, res) => {
 app.get('/emp_list.ajax', (req, res) => {
     const list = daoEmp.selectList();
     res.json(list);
+});
+
+app.get('/emp_detail.ajax', (req, res) => {
+    const id = req.query.e_id;
+    const vo = daoEmp.selectOne(id);
+    res.json(vo);
+});
+
+
+app.post('/emp_add.ajax', (req, res) => {
+    daoEmp.insert(req.body);
+    res.json({success: true});
+});
+
+app.post('/emp_mod.ajax', (req, res) => {
+    const cnt = daoEmp.update(req.body);
+    res.json({ success: cnt === 1, cnt });
+});
+app.post('/emp_del.ajax', (req, res) => {
+    const cnt = daoEmp.delete(req.body.e_id);
+    res.json({ cnt });
 });
 
 // 서버 시작
